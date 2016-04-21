033ae099a4e38c4b14556ca1e095619843d7cee7
iamthemananiket <iamthemananiket@gmail.com>
2016-04-21 10:36:32 +0530

    changed php

--- codeEditor/index.php
+++ codeEditor/index.php
@@ -3,28 +3,38 @@ ob_start();
 ?>
 <pre>
 <?php
+function parse($text) {
+    // Damn pesky carriage returns...
+    $text = str_replace("\r\n", "\n", $text);
+    $text = str_replace("\r", "\n", $text);
+
+    // JSON requires new line characters be escaped
+    $text = str_replace("\n", "\\n", $text);
+    return $text;
+}
  // header("Content-Type: text/plain");
 function get_project_data($project){
+	// print_r($project);
 	$project_contents = array();
 	foreach ($project as $key => $value) {
-		$data = $key.':'.'';
-		$files = explode(',', $value);
-		$file_and_data = array();
-		for ($i=0; $i < count($files); $i++) { 
-			if(file_exists($files[$i])){
-				$fd = fopen($files[$i], "r");
+		for ($i=0; $i < count($value); $i++) {
+			$file = $project[$key][$i];
+			$file_and_data = array(); 
+			if(file_exists($file)){
+				$fd = fopen($file, "r");
 				$content = "";
 				while($s=fread($fd, 100)){
 					 // echo $s;
 					$content = $content.$s;
 				}
-				$file_and_data[$files[$i]] = $content;
+				$file_and_data[$file] = $content;
+				$project[$key][$i] = $file_and_data;
 			}
 		}
-		$project_contents[$key] = $file_and_data;
 	}
+	// print_r($project);
 	// echo json_encode($project_contents);
-	echo (json_encode($project_contents)) ;
+	echo (json_encode($project)) ;
 }
 ?>
 <?php
@@ -41,48 +51,78 @@ function get_project_data($project){
 				array_pop($path_list);
 				foreach ($files as $key => $value) {
 					if($value != '.' and $value != '..'){
+						// echo($value."\n");
 						if(ereg(".+\.js", $value))	{
-							$project_files["js"] = $project_files["js"].$parent_dir.'/'.$value.',';
+							$project_files["js"] = $project_files["js"].$parent_dir.'/'.$value.'&';
 						}
 						if(ereg(".+\.css", $value))	{
-							$project_files["css"] = $project_files["css"].$parent_dir.'/'.$value.',';
+							$project_files["css"] = $project_files["css"].$parent_dir.'/'.$value.'&';
 						}
 						if(ereg(".+\.html", $value))	{
-							$project_files["html"] = $project_files["html"].$parent_dir.'/'.$value.',';
+							$project_files["html"] = $project_files["html"].$parent_dir.'/'.$value.'&';
 						}
 						if(is_dir($parent_dir.'/'.$value))
 							array_push($path_list, $parent_dir.'/'.$value);												
 					}
 				}
 				// echo "Path List";
-				// print_r($path_list);
+				 // print_r($path_list);
 			}
 			$project_value = "";
 			// print_r($project_files);
 			foreach ($project_files as $key => $value) {
+				// echo $key.":".$value;
 				$value = substr($value, 0, strlen($value) - 1);
-				$project_value = $project_value.$key.'='.$value.'&';
+				$value_array = explode('&', $value);
+				for ($i=0; $i < count($value_array); $i++) { 
+					$project_value = $project_value.$key.'[]='.$value_array[$i].'&';
+				}
+					
 			}
 			$project_value = substr($project_value, 0, strlen($project_value) - 1);
-			// echo $project_value;
+			// echo $project_value.'\n';
 			parse_str($project_value, $temp);
 			// print_r($temp);
-			if(setcookie($project, $project_value, time()+1000000, '/'))
+			if(setcookie('project', $project_value, 0, '/'))
 				;// ;echo "cookie set";
 			else
 				echo "Not set";
+			// echo $temp;
 			get_project_data($temp);
 
 		}else{
 			echo "Invalid Path".$project;
 		}
 	}
-	if(isset($_COOKIE['project']) &&isset($_GET['code'])){
-		echo $_COOKIE['project'];
-		echo exec('whoami');
-		$fd = fopen('temp.html', "w");
-		echo fwrite($fd, $_GET["code"]);
-		fclose($fd);
+	if(isset($_GET['code'])){
+		parse_str($_COOKIE['project'], $temp);
+		// print_r($temp);
+		$content = json_decode(parse($_GET['code']), true);
+		// var_dump($temp);
+		// print_r($content);
+		foreach ($content as $i => $j) {
+			foreach ($j as $k => $l) {
+				foreach ($l as $key => $value) {
+					$dirs = explode('/', $key);
+					$path = "";
+					if(count($dirs) > 1){
+						$path = implode('/', array_slice($dirs, 0, $length = count($dirs)- 1));
+						echo $path;
+						echo "<><><><<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>";
+
+						if(!is_dir($path) && !file_exists($key))
+							mkdir ($path , $mode = 0777 , $recursive = true );
+					}
+					$fd = fopen($key, "w");
+					fwrite($fd, $value);
+					fclose($fd);
+					# code...
+				}
+				# code...
+			}
+
+		}
+
 	}
 	ob_flush();
 ?>
